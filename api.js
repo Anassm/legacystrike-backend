eee8ea1418b4445d06f33dd6abe3cfdfbc6f7aa7
Anass <a.moussadi03@gmail.com>
2023-11-05 20:39:52 +0100

    Added extra file filter incase directory is used for both demos and json files. Changed variable to be based on env.

--- api.js
+++ api.js
@@ -1,28 +1,29 @@
- const fs = require("fs");
+const fs = require("fs");
 const { env } = require("process");
 
 // Endpoint - Return most recent 5 parsed demo files.
 const getRecentGames = (res) => {
-  const fileDirectory = env.DEMO_JSON_DIRECTORY;
+  const fileDirectory = env.DEMO_DIRECTORY;
 
   fs.readdir(fileDirectory, (err, files) => {
     if (err) {
       return res.status(500).send(`${err}`);
     }
 
-    const jsonFiles = files.slice(-5);
+    const jsonFiles = files.filter((file) => file.endsWith(".json"));
 
-    const finalResponse = jsonFiles.map((file) => readFile(file));
+    const recentJsonFiles = jsonFiles.slice(-5);
+
+    const finalResponse = recentJsonFiles.map((file) => readFile(file));
 
     res.json(finalResponse);
   });
 };
 
 function readFile(fileName) {
-  const dir = env.DEMO_JSON_DIRECTORY;
+  const dir = env.DEMO_DIRECTORY;
   try {
-    const data = fs.readFileSync(`${dir}/${fileName}`, 'utf8');
-    console.log(data);
+    const data = fs.readFileSync(`${dir}/${fileName}`, "utf8");
     return JSON.parse(data);
   } catch (err) {
     console.error(err);
